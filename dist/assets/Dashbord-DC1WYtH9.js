006a6c25a74540246713d4ec354a7a14f8b29929
mokshali <mokshalibawiskar16@gmail.com>
2025-01-24 09:52:58 +0530

    add dist

--- dist/assets/Dashbord-DC1WYtH9.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-yUr1FVeW.js";const o=()=>r.jsx("div",{children:"Dashbord"});export{o as default};
---
+++ dist/assets/Dashbord-eS6Tynjd.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Bd1EFJwK.js";const o=()=>r.jsx("div",{children:"Dashbord"});export{o as default};
