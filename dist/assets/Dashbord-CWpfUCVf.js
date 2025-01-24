dc9650b1749722eb685c41a28dd954bf771c1f07
mokshali <mokshalibawiskar16@gmail.com>
2025-01-24 09:25:51 +0530

    add dist

--- dist/assets/Dashbord-CWpfUCVf.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-D8O8_SQL.js";const o=()=>r.jsx("div",{children:"Dashbord"});export{o as default};
---
+++ dist/assets/Dashbord-DC1WYtH9.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-yUr1FVeW.js";const o=()=>r.jsx("div",{children:"Dashbord"});export{o as default};
